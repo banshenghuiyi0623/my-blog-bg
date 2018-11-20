25cabfc0d00950dc8e6ae2bed66c0ed15ca2711a
gaofeng <gaofengmac@gaofengdeMacBook-Pro.local>
2018-11-20 22:27:28 +0800

    实现图片资源请求

--- src/servers/static-resource-server/controllers/getStatic.ts
+++ src/servers/static-resource-server/controllers/getStatic.ts
@@ -15,67 +15,36 @@ function parseMime (url: string) {
 
   return  mimes[extName]
 }
-// 文件不存在处理
-function noFileHandler (ctx: Koa.Context) {
-  ctx.res.writeHead(404, {
-    'Content-Type': 'text/plain'
-  });
-  ctx.res.write(`This request URL ${ctx.url} was not found`)
-  ctx.res.end()
-}
-// 文件存在处理
-function fileHandler (ctx: Koa.Context, file: any) {
-  let _mime = parseMime( ctx.url )
 
-  // 如果有对应的文件类型，就配置上下文的类型
-  const contentType = _mime || "text/plain"
-  
-  ctx.res.writeHead(200, {
-    'Content-Type': contentType
-  })
-  ctx.res.write(file, 'binary')
-  ctx.res.end()
-}
-// 错误处理
-function errorHandler (ctx: Koa.Context) {
-  ctx.res.writeHead(500, {
-    'Content-Type': 'text/plain'
-  })
-  ctx.res.write('服务器错误')
-  ctx.res.end()
-}
-
-// async function getStatic(ctx: Koa.Context) {
-//   let fullStaticPath = path.join(__dirname, staticPath)
-//   let _content = await getContent ( ctx, fullStaticPath )
-//   let _mime = parseMime( ctx.url )
-
-//   // 如果有对应的文件类型，就配置上下文的类型
-//   ctx.type = _mime || "text/plain"
-
-//   if ( _mime ) {
-//     ctx.res.writeHead(200)
-//     ctx.res.write(_content, 'binary')
-//     ctx.res.end()
-//   }
-// }
-function getStatic(ctx: Koa.Context) {
-  let fullStaticPath = path.join(__dirname, staticPath)
-  let reqPath = path.join(fullStaticPath, ctx.url)
-  fs.exists(reqPath, (isExists: boolean) => {
-    if (!isExists) {
-      noFileHandler(ctx)
-    } else {
-      fs.readFile(reqPath, (err: any, file: any) => {
-        if (err) {
-          errorHandler(ctx)
-        } else {
-          console.log('文件存在')
-          fileHandler(ctx, file)
-        }
-      })
-    }
+function getFile(path: string) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      err && reject(err)
+      resolve(data)
+    })
   })
 }
-  
-module.exports = getStatic 
+
+function checkFile(path: string) {
+  return new Promise((resolve, reject) => {
+    fs.exists(path, exists => {
+      exists && resolve(exists)
+      reject()
+    })
+  })
+}
+
+async function getStatic(ctx: Koa.Context) {
+  console.log('getstatic')
+  const fullStaticPath = path.join(__dirname, staticPath)
+  const reqPath = path.join(fullStaticPath, ctx.url)
+  const exists = await checkFile(reqPath)
+  const content = exists ? await getFile(reqPath) : 'sorry can`t find file'
+  const _mime = parseMime( ctx.url )
+  ctx.type = _mime || "text/plain"
+  ctx.res.writeHead(200)
+  ctx.res.write(content, 'binary')
+  ctx.res.end()
+}
+
+module.exports = getStatic
